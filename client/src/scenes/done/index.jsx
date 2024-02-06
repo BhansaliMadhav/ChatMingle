97ec2a65598bfbba79c9cd09c1a7f2b8c65527d2
MadhavBhansali <bhansalimadhav03@gmail.com>
2024-02-06 17:02:10 +0530

    resolved the errors in client causing the app to crash

--- client/src/scenes/done/index.jsx
+++ client/src/scenes/done/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import jwt from "jsonwebtoken";
+import { useNavigate } from "react-router-dom";
 const Done = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const tokenCode = localStorage.getItem("tokenCode");
     const user = jwt.decode(tokenCode);
